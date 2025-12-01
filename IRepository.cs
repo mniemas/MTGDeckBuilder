877c5fd54720bef16b589b478e3baef7a500125c
Ramona Angle <pangle1@uncc.edu>
2025-12-01 13:24:15 -0500

    repo stuff

---
+++ IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace MTGDeckBuilder;
+
+public interface IRepository
+{
+    public Card Search(string CardName);
+
+    public List<Card> Filter(bool[] Filter, string[] Query, Card CardTypeQuery, FilterSortTemplate SortMethod);
+    
+}
